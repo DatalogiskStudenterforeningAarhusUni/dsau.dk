f22784e22e702c96e261ea02b53b69d3ac56d8af
Jeppe K <jeppe_kuk@hotmail.com>
2019-09-12 15:36:48 +0200

    Updates

---
+++ precache-manifest.6ea50bf2dd48eb6f8bd312bf4bf9c9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7e70f700cb5754d3df582525a2c09d",
+    "url": "/dsau.dk/index.html"
+  },
+  {
+    "revision": "91c630463fb7eb862913",
+    "url": "/dsau.dk/static/css/2.48adc591.chunk.css"
+  },
+  {
+    "revision": "1cc94d4e1a771536aab3",
+    "url": "/dsau.dk/static/css/main.e730801c.chunk.css"
+  },
+  {
+    "revision": "91c630463fb7eb862913",
+    "url": "/dsau.dk/static/js/2.bd46f2e3.chunk.js"
+  },
+  {
+    "revision": "1cc94d4e1a771536aab3",
+    "url": "/dsau.dk/static/js/main.4767531f.chunk.js"
+  },
+  {
+    "revision": "617cf421db0e8bb7c397",
+    "url": "/dsau.dk/static/js/runtime~main.74967c90.js"
+  }
+]);
