5fb9a74aebefaf8de42fa8be703e7ff40315405a
Christian Zhuang-Qing Nielsen <Cznielsen@github.com>
2019-10-09 17:24:10 +0200

    update few text fields and add flavour text

--- src/pages.js
+++ src/pages.js
@@ -1,13 +1,39 @@
 import React from "react";
 
 export function Index() {
-    return <h2>Home</h2>;
+    return (
+        <div>
+            <h1>Datalogisk studenderforening på Aarhus Universitet</h1>
+            <div>
+                <h3>Hjertelig velkommen til DSAU's hjemmeside.</h3>
+                <div>
+                    Her kan du blandt andet finde vores vedtægter og seneste referater, 
+                    se adresse og kontaktinformation, se nuværende bestyrelse og læse om de årlige samt ekstraordinære events
+                    der bliver afholdt i studenterforeningen.
+                </div>
+            </div>
+        </div>
+    );
 }
 
+//TODO add more to about
 export function About() {
-    return <h2>Om</h2>;
+    return (
+        <div>
+            <h1>Om DSAU</h1>
+            <div>
+                <b>D</b>atalogisk <b>S</b>tudenterforening på <b>A</b>arhus <b>U</b>niversitet er en studenterforening for datalogistuderende på AU.
+                Foreningens opgave er at formidle idéer og forslag fra datalogistuderende til universitet, samt stå for aktiviterer af social- og faglig karakter.
+            </div>
+        </div>
+    );
 }
 
 export function Contact() {
-    return <h2>Kontakt os</h2>
+    return (
+        <div>
+            <h1>Kontakt os</h1>
+            <h3>Mail: </h3> 
+        </div>
+    );
 }
