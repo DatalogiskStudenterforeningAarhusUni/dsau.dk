8210e2e8cbbd8b027a47e1d77d64fd61a2e84da8
Søren Eriksen <soeren.skov.eriksen@lego.com>
2020-01-28 16:27:49 +0100

    Updates

---
+++ precache-manifest.22944d08e359186de92fc1461dfe8488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73024d06a97eed89ae32a192e9d89267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e6a833b6a20a4c16f87",
+    "url": "/static/css/2.48adc591.chunk.css"
+  },
+  {
+    "revision": "847c38a69c5511b093b9",
+    "url": "/static/css/main.ad97f67b.chunk.css"
+  },
+  {
+    "revision": "6e6a833b6a20a4c16f87",
+    "url": "/static/js/2.1d9fb620.chunk.js"
+  },
+  {
+    "revision": "847c38a69c5511b093b9",
+    "url": "/static/js/main.39b574c2.chunk.js"
+  },
+  {
+    "revision": "e86f0a9932f328ff8fe8",
+    "url": "/static/js/runtime~main.58706c16.js"
+  }
+]);
