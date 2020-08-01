feca48cf922274e4dd70ab6f98e5ca87a579c111
soer7022 <soer7022@users.noreply.github.com>
2020-08-01 12:22:28 +0000

    deploy: 8a154956eaacd69b281a61ac52c6f37197da88cd

---
+++ precache-manifest.09fcf4d77c4dbaac9afca2722c26b078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615ccf3386b644742b72680e56d89af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3398b1df3ce6b424fb3a",
+    "url": "/static/css/2.48adc591.chunk.css"
+  },
+  {
+    "revision": "4e6f1e52855465f650cb",
+    "url": "/static/css/main.7561dfb7.chunk.css"
+  },
+  {
+    "revision": "3398b1df3ce6b424fb3a",
+    "url": "/static/js/2.a15c9831.chunk.js"
+  },
+  {
+    "revision": "777de85d57e3e9adf28657bbb7a9c948",
+    "url": "/static/js/2.a15c9831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6f1e52855465f650cb",
+    "url": "/static/js/main.9ce9a6d8.chunk.js"
+  },
+  {
+    "revision": "df26286e42c53d5a839f",
+    "url": "/static/js/runtime-main.0a301424.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bf1b0abf875d83f4e9011bfd5eb7cda.js"
+  "/precache-manifest.09fcf4d77c4dbaac9afca2722c26b078.js"
 );
 
 self.addEventListener('message', (event) => {
