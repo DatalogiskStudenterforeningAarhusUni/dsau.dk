49a1db719a9fee5704e9804602ef11d69e417d20
Anders <Anders@bjerrumjensen.dk>
2025-12-03 10:36:09 +0100

    datoer for klan og lnsc

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
