620d957037fb541c3877fc07dc82180e013c4b59
AndersJensenStudie <anders.jensen.studie@gmail.com>
2026-05-12 11:13:04 +0200

    fix: now builds successfully

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -8,7 +8,7 @@ interface MemberInfo {
   imageFocus?: number
 }
 
-export default function Member({ name, image, title, email, class: classYear, imagePosition, imageFocus }: MemberInfo) {
+export default function Member({ name, image, title, email, class: imagePosition, imageFocus }: MemberInfo) {
   const positionStyle =
     imagePosition
       ? { objectPosition: imagePosition }
